c32d28171afe913a5b863040e7dc1ca3916e3c26
icsalgado <icsalgado@icloud.com>
2021-10-31 17:10:46 -0300

    Renderizando elementos como components no React

--- React/intro-reactjs/src/index.js
+++ React/intro-reactjs/src/index.js
@@ -10,15 +10,22 @@ function primeiroJSX(){
     )
 }
 
+const element = 'Teste elemento 1 que pode ser um string';
+const element2 = <h1>Teste elemento 2 que pode ser um html</h1>;
+
 const App = () => {
 
     return(
         <div className="App">
             Hello World
             {primeiroJSX()}
+            {element}
+            {element2}
         </div>
     )
 }
+
+
 
 const rootElement = document.getElementById("root");
 ReactDom.render(<App />, rootElement);
