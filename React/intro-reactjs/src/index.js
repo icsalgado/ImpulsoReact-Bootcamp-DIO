7d18822ffa46192bba292ee94f4b80556748bda9
icsalgado <icsalgado@icloud.com>
2021-10-31 17:44:13 -0300

    Entendendo o ciclo de vida dos componentes

--- React/intro-reactjs/src/index.js
+++ React/intro-reactjs/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDom from "react-dom";
 import "./styles.css";
 import "./Button";
@@ -15,6 +15,37 @@ function primeiroJSX(){
 const element = 'Teste elemento 1 que pode ser um string';
 const element2 = <h1>Teste elemento 2 que pode ser um html</h1>;
 
+class App extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            clock: 1000,
+            copo: 'agua'
+        }
+    }
+
+    alterarCopo = () => {
+        this.setState({
+            copo: 'refrigerante'
+        })
+
+    } 
+
+    render(){
+        const { clock, copo } = this.state
+
+        return(
+            <div>
+                <h1>{ clock }</h1>
+                <button onClick={() => this.alterarCopo()}>{copo}</button>
+            </div>
+        )
+    }
+}
+
+/*
 const App = () => {
 
     return(
@@ -28,7 +59,7 @@ const App = () => {
     )
 }
 
-
+*/
 
 const rootElement = document.getElementById("root");
 ReactDom.render(<App />, rootElement);
